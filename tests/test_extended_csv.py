e98a541a504fd5464444d70946bf77a1dc59b0b4
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-11-11 18:46:45 +0700

    Add tests for extended_csv.py
    - Test specifying lines read

---
+++ tests/test_extended_csv.py
@@ -0,0 +1,25 @@
+import unittest
+
+from extended_csv import read_xsv_file
+
+TestCase = unittest.TestCase
+
+
+class MyTestCase(TestCase):
+
+    def test_read_xsv_line_spec(self):
+        test_cases = [
+            ('nutrient-100.csv', None, 80, 20),
+            # TODO: maybe automatically test time elapsed as well
+            ('branded_food-10000.csv', 10, 10, None),
+            ('sr_legacy_food-150.csv', 200, 130, 20),
+            ('food_category-28.csv', 1, 1, 0),
+        ]
+        for file, to_read, read, skip in test_cases:
+            with self.subTest(f"{file=},{to_read=},{read=},{skip=}"):
+                result = read_xsv_file(f'data/csv/{file}', dialect='excel', load_at_most=to_read, discard=skip)
+                self.assertEqual(read, len(result))
+
+
+if __name__ == '__main__':
+    unittest.main()
