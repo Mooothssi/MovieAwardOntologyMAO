a6c702c1262488f51c73b919833a939f7db9b1a8
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-11-14 16:35:46 +0700

    Update tests for autowrite models

--- tests/test_autowrite.py
+++ tests/test_autowrite.py
@@ -1,29 +1,30 @@
 import unittest
-import pandas as pd
+from io import StringIO
 
+from extended_csv import read_xsv_file
+from sa_autowrite.__main__ import write_model
 
 TestCase = unittest.TestCase
 
 
 class TestAutowrite(TestCase):
 
-    def test_something(self):
-        with open('data/autowrite/nutrient.py', 'r', encoding='utf-8') as f:
-            expected_result = f.read()
-        options = {
-            'lines_to_consider': 100,
-            'primary_key_columns': None,
-        }
-        self.assertEqual(expected_result, somefunction('data/csv/nutrient-100.csv', **options))
+    def test_write_model(self):
+        args = [
+            ('Nutrient', 'autowrite/nutrient.py', 'csv/nutrient-100.csv', 100, {'pk_cols': ['id']}),
+            ('BrandedFood', 'autowrite/branded_food.py', 'csv/branded_food-10000.csv', 10000, {}),
+        ]
+        for name, out, in_, limit, options in args:
+            # Commented out because PyCharm doesn't support auto show large diff in subTests yet
+            # with self.subTest(f'{out=}{in_=}{limit=}{options=}'):
+            with open(f'data/{out}', 'r', encoding='utf-8') as f:
+                expected_result = f.read()
 
-    def test_report(self):
-        with open('data/autowrite/nutrient.py', 'r', encoding='utf-8') as f:
-            expected_result = f.read()
-        options = {
-            'discard': 0,
-            'max_lines': 100,
-        }
-        self.assertEqual(expected_result, report('data/csv/nutrient-100.csv', **options))
+            output = StringIO()
+            data = read_xsv_file(f'data/{in_}', dialect='excel', load_at_most=limit)
+            write_model(output, name, data, **options)
+
+            self.assertEqual(expected_result, output.getvalue())
 
 
 if __name__ == '__main__':
