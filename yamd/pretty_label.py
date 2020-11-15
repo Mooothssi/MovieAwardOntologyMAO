492cd6774d244b4b8390c96477202fc5a347c7ca
Nutthanich Narphromar <monaprom134@gmail.com>
2020-11-15 22:48:40 +0700

    Complete pretty_label function

--- yamd/pretty_label.py
+++ yamd/pretty_label.py
@@ -1,3 +1,5 @@
+import re
+
 __all__ = ['get_pretty_label', 'get_language_from_code']
 
 
@@ -8,8 +10,8 @@ def get_pretty_label(ugly_label: str) -> str:
         ugly_label: The ugly machine label.
 
     Examples:
-            >>> get_pretty_label('rdfs:label')
-        'label'
+        >>> get_pretty_label('rdfs:label')
+        'Label'
         >>> get_pretty_label('owl:disjointWith')
         'Disjoint With'
         >>> get_pretty_label('annotations')
@@ -22,7 +24,7 @@ def get_pretty_label(ugly_label: str) -> str:
         'Object Property'
         >>> get_pretty_label('dataProperty')
         'Data Property'
-        >>> get_pretty_label('equivalentClass')
+        >>> get_pretty_label('owl:equivalentClass')
         'Equivalent To'
         >>> get_pretty_label('dc:title')
         'Title'
@@ -49,19 +51,41 @@ def get_pretty_label(ugly_label: str) -> str:
         >>> get_pretty_label('rules')
         'Rules'
         >>> get_pretty_label('owl:AnnotationProperty')
-        'Annotation property'
+        'Annotation Property'
         >>> get_pretty_label('rdfs:Datatype')
         'Data type'
     """
-    if ugly_label == 'rdfs:label':
-        return 'Label'
-    if ugly_label == 'dc:title':
-        return 'Title'
-    if ugly_label == 'dcterms:licence':
-        return 'Licence'
-    if ugly_label == 'rdfs:comment':
-        return 'Comments'
-    raise AssertionError(ugly_label)
+    prefixes = ['rdfs', 'rdf', 'owl', 'dc', 'dcterms']
+    exceptions = ['owl:topDataProperty', 'owl:topObjectProperty']
+    sections = ['objectProperty', 'dataProperty']
+    prefix_label = ugly_label.split(':')
+
+    if ugly_label in exceptions:
+        return prefix_label[1]
+
+    if len(prefix_label) > 1:
+        if prefix_label[1] == 'equivalentClass':
+            return 'Equivalent To'
+        elif prefix_label[1] == 'subClassOf':
+            return 'Subclass Of'
+
+    if (prefix_label[0] in prefixes) or (prefix_label[0] in sections):
+        label = prefix_label[-1]
+        label = re.sub(r'(?<!^)(?=[A-Z])', ' ', label)
+        pretty_label = label.title()
+        if pretty_label == 'Restriction':
+            return ''
+        elif pretty_label == 'Type':
+            return 'Characteristics'
+        elif pretty_label == 'Datatype':
+            return 'Data type'
+        elif pretty_label == 'Equivalent Class':
+            return 'Equivalent To'
+        return pretty_label
+    else:
+        if len(prefix_label) > 1:
+            return prefix_label[1]
+        return prefix_label[0].title()
 
 
 def get_language_from_code(language_code: str) -> str:
