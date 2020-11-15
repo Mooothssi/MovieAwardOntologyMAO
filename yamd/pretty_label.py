88d623b083b2f4bed71c793bce299c6cade0b9e3
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-11-15 20:20:32 +0700

    Add skeleton for `get_pretty_label`

--- yamd/pretty_label.py
+++ yamd/pretty_label.py
@@ -1,3 +1,18 @@
 MAP = {
     'rdfs:label': 'label'
 }
+
+
+def get_pretty_label(ugly_label: str) -> str:
+    """Returns a pretty human-readable label
+
+    Args:
+        ugly_label: The ugly machine label.
+
+    Examples:
+        >>> get_pretty_label('rdfs:label')
+        'label'
+        >>> get_pretty_label('owl:disjointWith')
+        'Disjoint With'
+    """
+    pass
