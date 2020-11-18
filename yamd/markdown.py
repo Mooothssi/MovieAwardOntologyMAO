a2520b605f2f0cea265e9ec2a2015571533522f9
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-11-18 09:02:45 +0700

    Fix get_md_list() to allow `map` as well.

--- yamd/markdown.py
+++ yamd/markdown.py
@@ -1,7 +1,7 @@
-from typing import Iterable, Union
+from typing import Sequence, Union
 
 
-def get_md_list(level: int, lst: Iterable[Union[str, Iterable[str]]]) -> str:
+def get_md_list(level: int, lst: Sequence[Union[str, Sequence[str]]]) -> str:
     """Returns a markdown list indented to the appropriate level.
 
     Args:
@@ -11,12 +11,16 @@ def get_md_list(level: int, lst: Iterable[Union[str, Iterable[str]]]) -> str:
     Examples:
         >>> get_md_list(5, [])
         ''
-        >>> get_md_list(0, ['Thing'])
+        >>> get_md_list(-1, ['Thing'])
         '- Thing'
-        >>> get_md_list(2, ['Pizza','Basil'])
+        >>> get_md_list(1, ['Pizza','Basil'])
         '    - Pizza\\n    - Basil'
         >>> get_md_list(0, ['Ant', ['Ant man', 'Ant woman']])
-        '- Ant\\n  - Ant man\\n  - Ant woman'
+        '  - Ant\\n    - Ant man\\n    - Ant woman'
+        >>> get_md_list(0, [{'@type': 'Person', 'name': 'Charlie'}, {}])
+        Traceback (most recent call last):
+          ...
+        TypeError: item must be a str or an iterable of str, not 'dict'
 
     Notes:
         Base list item must be `str` in order to support
@@ -26,6 +30,12 @@ def get_md_list(level: int, lst: Iterable[Union[str, Iterable[str]]]) -> str:
     """
     if isinstance(lst, str):
         return ''.join(('  ' * level, '- ', lst))
-    if isinstance(lst, Iterable):
+    if isinstance(lst, (Sequence, map)):
         return '\n'.join(get_md_list(level + 1, item) for item in lst)
-    raise TypeError(f"item must be a str or an iterable of str, not {lst.__class__.__name__}")
+    raise TypeError(f"item must be a str or an iterable of str, not '{lst.__class__.__name__}'")
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
