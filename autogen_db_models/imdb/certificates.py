b7fb884f7a92780ae2da9ecf627a7647a4058cfa
Ding <ding533@hotmail.com>
2020-11-22 16:53:56 +0700

    map data

--- autogen_db_models/imdb/certificates.py
+++ autogen_db_models/imdb/certificates.py
@@ -11,9 +11,20 @@ class Certificates(Base):
     year = Column(Integer)
     roman = Column(String)
     type = Column(String)
-    episodeInfo = Column(String)
+    episode_info = Column(String)
     certificate = Column(String)
 
     def __repr__(self):
-        return f"<Certificates(id='{self.id}', title='{self.title}', year='{self.year}', roman='{self.roman}', type='{self.type}', episodeInfo='{self.episodeInfo}', certificate='{self.certificate}')>"
+        return f"<Certificates(id='{self.id}', title='{self.title}', year='{self.year}', roman='{self.roman}', type='{self.type}', episode_info ='{self.episode_info }', certificate='{self.certificate}')>"
 
+    @classmethod
+    def create_instance(cls, row: 'Series'):
+        data = {
+            'title': row.get('title'),
+            'year': row.get('year'),
+            'roman': row.get('roman'),
+            'type': row.get('type'),
+            'episode_info ': row.get('episode_info'),
+            'certificate': row.get('data'),
+        }
+        return cls(**data)
--- autogen_db_models/imdb/production_companies.py
+++ autogen_db_models/imdb/production_companies.py
@@ -11,9 +11,20 @@ class ProductionCompanies(Base):
     year = Column(Integer)
     roman = Column(String)
     type = Column(String)
-    episodeInfo = Column(String)
-    productionCompany = Column(String)
+    episode_info = Column(String)
+    production_company = Column(String)
 
     def __repr__(self):
-        return f"<ProductionCompanies(id='{self.id}', title='{self.title}', year='{self.year}', roman='{self.roman}', type='{self.type}', episodeInfo='{self.episodeInfo}', productionCompany='{self.productionCompany}')>"
+        return f"<ProductionCompanies(id='{self.id}', title='{self.title}', year='{self.year}', roman='{self.roman}', type='{self.type}', episode_info ='{self.episode_info }', production_company='{self.production_company}')>"
 
+    @classmethod
+    def create_instance(cls, row: 'Series'):
+        data = {
+            'title': row.get('title'),
+            'year': row.get('year'),
+            'roman': row.get('roman'),
+            'type': row.get('type'),
+            'episode_info ': row.get('episode_info'),
+            'production_company': row.get('data'),
+        }
+        return cls(**data)
