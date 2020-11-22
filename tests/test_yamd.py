2091df8674f7e4cb887e46fa875d1d61e3194dbf
Nutthanich Narphromar <monaprom134@gmail.com>
2020-11-22 13:37:55 +0700

    Change file paths

--- tests/test_yamd.py
+++ tests/test_yamd.py
@@ -13,15 +13,16 @@ def load_tests(loader: unittest.TestLoader, tests: unittest.TestSuite, ignore) -
 
 class TestOwlYamlMd(unittest.TestCase):
     def test_main_test_cases(self):
+        version = 'v2.1.0'
         specs = [
             ('test_case1.yaml', 'test_case1.md'),
             ('test_case2.yaml', 'test_case2.md')
         ]
         for infile, outfile in specs:
-            with open(ROOT_DIR / f'tests/test_cases/{outfile}') as mdfile:
+            with open(ROOT_DIR / f'tests/specs/{version}/{outfile}') as mdfile:
                 expected_result = mdfile.read()
             result = StringIO()
-            main.convert_owl_yaml_to_md(ROOT_DIR / f'tests/test_cases/{infile}', result)
+            main.convert_owl_yaml_to_md(ROOT_DIR / f'tests/specs/{version}/{infile}', result)
             self.assertEqual(expected_result, result.getvalue())
 
 
