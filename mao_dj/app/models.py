45140d0d8ea8d4d2952bc21d46ec79aa89f5e623
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-12-20 18:04:40 +0700

    Update models.py
    - Add data retrieval from `wikidata`
    - Add UpsertMixin to some models
    
    Co-Authored-By: Tharathorn Bunrattanasathian <54201254+th-bunratta@users.noreply.github.com>
    Co-Authored-By: Nutthanich Narphromar <48724051+NutthanichN@users.noreply.github.com>

--- mao_dj/app/models.py
+++ mao_dj/app/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-
+import typing
+import re
 
 # class MaxLenMixin:
 #     @property
 #     def max_len(self):
-#         return max(map(self.choices))
+#         return max(map(self.choices, null=True), null=True)
+from django.db.models import ManyToOneRel, AutoField, ForeignObjectRel, Field
+
+from dirs import ROOT_DIR
+from mapping import OSCAR_MAPPING
+from utils.dict import select_not_null
+from wikidata_queries.base import get_content_ratings_for_film, get_single_valued_prop
 
 
 class Gender(models.TextChoices):
@@ -19,41 +26,102 @@ class CharacterImportance(models.TextChoices):
     EXTRA = 'extra'
 
 
+_M = typing.TypeVar('_M', bound=models.Model)
+
+
+class UpsertMixin:
+    def __init__(self, *arg, **kwargs):
+        super().__init__(*arg, **kwargs)
+
+    @classmethod
+    def upsert(cls: typing.Type[_M], **kwargs) -> _M:
+        kwds = {}
+        for rel in cls._meta.get_fields():
+            rel: typing.Union[ManyToOneRel, AutoField]
+            if isinstance(rel, Field):
+                field = rel.name
+            else:
+                field = rel.field.name
+            if '.' in field:
+                field = field.split('.')[-1]
+            v = kwargs.get(field)
+            if v is not None:
+                kwds[field] = v
+        try:
+            return cls.objects.get(**kwds)
+        except cls.DoesNotExist:
+            pass
+        return cls.objects.create(**kwds)
+
+
+class Agent(UpsertMixin, models.Model):
+    # temp
+    # label = models.CharField(max_length=255)
+
+    # class Meta:
+    #     abstract = True
+
+    @classmethod
+    def upsert_instance(cls,
+                        hasAward: 'Award' = None,
+                        yearHeld: str = None,
+                        hasEditionNumber: int = None,
+                        yearScreened: int = None
+                        ) -> 'AwardCeremony':
+        kwds = {
+            'hasAward': hasAward,
+            'yearHeld': yearHeld,
+            'hasEditionNumber': hasEditionNumber,
+            'yearScreened': yearScreened
+        }
+        try:
+            return cls.objects.get(**select_not_null(kwds))
+        except cls.DoesNotExist:
+            pass
+        return AwardCeremony.objects.create(**select_not_null(kwds))
+
+
 class Occupation(models.Model):
     # object properties
-    isOccupationof = models.ForeignKey('Person', on_delete=models.CASCADE)
+    isOccupationof = models.ForeignKey('Person', on_delete=models.CASCADE, null=True)
+
+    # temp
+    label = models.CharField(max_length=255)
 
 
-class Person(models.Model):
+class Person(Agent):
     # object properties
-    actsIn = models.ForeignKey('FilmMakingSituation', on_delete=models.CASCADE)
-    hasGender = models.CharField(max_length=255, choices=Gender.choices)
-    hasOccupation = models.ForeignKey(Occupation, on_delete=models.CASCADE)
-    eligibleFor = models.ForeignKey('Award', on_delete=models.CASCADE)
+    isParticipantIn = models.ForeignKey('FilmMakingSituation', on_delete=models.CASCADE, null=True)
+    hasGender = models.CharField(max_length=255, choices=Gender.choices, null=True)
+    hasOccupation = models.ForeignKey(Occupation, on_delete=models.CASCADE, null=True)
+    eligibleFor = models.ForeignKey('Award', on_delete=models.CASCADE, null=True)
 
     # data properties
-    hasName = models.CharField(max_length=255)
+    hasName = models.CharField(max_length=255, null=True)
+
+    # temp
+    n_const = models.CharField(max_length=255, unique=True, null=True)
 
 
 class CollectiveAgent(models.Model):
     # object properties
-    # isParticipantIn = models.ForeignKey('Situation', on_delete=models.CASCADE)
-    hasMember = models.ForeignKey(Person, on_delete=models.CASCADE)
+    # isParticipantIn = models.ForeignKey('Situation', on_delete=models.CASCADE, null=True)
+    hasMember = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
 
 
 class FilmCast(CollectiveAgent):
     # object properties
-    isParticipantIn = models.ForeignKey('ActingSituation', on_delete=models.CASCADE)
+    isParticipantIn = models.ForeignKey('ActingSituation', on_delete=models.CASCADE, null=True)
 
 
 class FilmCrew(CollectiveAgent):
     # object properties
-    isParticipantIn = models.ForeignKey('FilmMakingSituation', on_delete=models.CASCADE)
+    isParticipantIn = models.ForeignKey('FilmMakingSituation', on_delete=models.CASCADE, null=True)
 
 
 class Situation(models.Model):
     # object properties
-    isSettingFor = models.ForeignKey('Award', on_delete=models.CASCADE)  # actually owl:Thing
+    isSettingFor = models.ForeignKey('Award', on_delete=models.CASCADE, null=True)  # actually owl:Thing
 
 
 class ActingSituation(Situation):
@@ -61,51 +129,66 @@ class ActingSituation(Situation):
     hasActor = models.ForeignKey(Person, on_delete=models.CASCADE)
     hasCharacter = models.ForeignKey('Character', on_delete=models.CASCADE)
     hasFilm = models.ForeignKey('Film', on_delete=models.CASCADE)
-    isPartOf = models.ForeignKey('FilmMakingSituation', on_delete=models.CASCADE)
+    isPartOf = models.ForeignKey('FilmMakingSituation', on_delete=models.CASCADE, null=True)
 
 
 class FilmMakingSituation(Situation):
     # object proerties
-    hasCast = models.ForeignKey(FilmCast, on_delete=models.CASCADE)
-    hasCinematographer = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='cinematographer_of_film_making_situation_set')
-    hasComposer = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='composer_of_film_making_situation_set')
-    hasCrew = models.ForeignKey(FilmCrew, on_delete=models.CASCADE)
-    hasDirector = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='director_of_film_making_situation_set')
+    hasCast = models.ForeignKey(FilmCast, on_delete=models.CASCADE, null=True)
+    hasCinematographer = models.ForeignKey(Person, on_delete=models.CASCADE,
+                                           related_name='cinematographer_of_film_making_situation_set', null=True)
+    hasComposer = models.ForeignKey(Person, on_delete=models.CASCADE,
+                                    related_name='composer_of_film_making_situation_set', null=True)
+    hasCrew = models.ForeignKey(FilmCrew, on_delete=models.CASCADE, null=True)
+    hasDirector = models.ForeignKey(Person, on_delete=models.CASCADE,
+                                    related_name='director_of_film_making_situation_set', null=True)
     hasFilm = models.ForeignKey('Film', on_delete=models.CASCADE)
-    hasPart = models.ForeignKey(ActingSituation, on_delete=models.CASCADE)
-    hasProducer = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='producer_of_film_making_situation_set')
+    hasPart = models.ForeignKey(ActingSituation, on_delete=models.CASCADE, null=True)
+    hasProducer = models.ForeignKey(Person, on_delete=models.CASCADE,
+                                    related_name='producer_of_film_making_situation_set', null=True)
 
 
-class NominationSituation(Situation):
+class NominationSituation(UpsertMixin, Situation):
     # object properties
     forFilm = models.ForeignKey('Film', on_delete=models.CASCADE)
     hasAward = models.ForeignKey('Award', on_delete=models.CASCADE)
     hasAwardCategory = models.ForeignKey('AwardCategory', on_delete=models.CASCADE)
     hasAwardCeremony = models.ForeignKey('AwardCeremony', on_delete=models.CASCADE)
-    isGivenTo = models.ForeignKey(Person, on_delete=models.CASCADE)
+    isGivenTo = models.ForeignKey(Agent, on_delete=models.CASCADE)
 
     # data properties
     win = models.BooleanField()
 
 
-class ContentRating(models.Model):
-    # data properties
+class ContentRatingCategory(UpsertMixin, models.Model):
+    hasValue = models.CharField(max_length=255)
+    isPartOf = models.OneToOneField('ContentRating', on_delete=models.CASCADE, related_name='hasPart')
+
+
+class ContentRating(UpsertMixin, models.Model):
     hasDescription = models.CharField(max_length=255)
+    # data properties
+    appliesInCountry = models.ForeignKey('Country', on_delete=models.PROTECT, null=True)
 
 
 class Place(models.Model):
     pass  # Nothing
 
 
-class Country(Place):
+class Country(Place, UpsertMixin):
     # annotation property
     label = models.CharField(max_length=255)
+    alpha_2 = models.CharField(max_length=2)
+    alpha_3 = models.CharField(max_length=3)
 
 
-class Genre(models.Model):
+class Genre(models.Model, UpsertMixin):
     # object properties
-    hasSubGenre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True, related_name='isSubGenreOf')
-    # isSubGenreOf = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True, related_name='SubGenreOf_set')
+    hasSubGenre = models.ForeignKey('Genre', on_delete=models.SET_NULL, related_name='isSubGenreOf', null=True)
+    # isSubGenreOf = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True, related_name='SubGenreOf_set', null=True)
+
+    # temp
+    label = models.CharField(max_length=255)
 
 
 class Language(models.Model):
@@ -113,81 +196,168 @@ class Language(models.Model):
     label = models.CharField(max_length=255)
 
 
-class AwardCeremony(models.Model):
+class AwardCeremony(UpsertMixin, models.Model):
     # object properties
-    follow = models.ForeignKey('AwardCeremony', on_delete=models.SET_NULL, null=True, related_name='followedBy')
-    # followedBy = models.ForeignKey('AwardCeremony', on_delete=models.SET_NULL, null=True)
-    hasAward = models.ForeignKey('Award', on_delete=models.CASCADE)
+    # follow = models.ForeignKey('AwardCeremony', on_delete=models.SET_NULL, related_name='followedBy', null=True)
+    # followedBy = models.ForeignKey('AwardCeremony', on_delete=models.SET_NULL, null=True, null=True)
+    hasAward = models.ForeignKey('Award', on_delete=models.CASCADE, null=True)
 
     # data properties
-    dateHeld = models.CharField(max_length=255)
+    # dateHeld = models.CharField(max_length=255)
+    yearHeld = models.IntegerField()
     hasEditionNumber = models.IntegerField()
     yearScreened = models.IntegerField()
 
+    @property
+    def follow(self) -> typing.Optional['AwardCeremony']:
+        try:
+            return self.objects.get(hasEditionNumber=self.hasEditionNumber - 1)
+        except self.DoesNotExist:
+            return None
 
-class AwardCategory(models.Model):
+    @property
+    def followedBy(self) -> typing.Optional['AwardCeremony']:
+        try:
+            return self.objects.get(hasEditionNumber=self.hasEditionNumber + 1)
+        except self.DoesNotExist:
+            return None
+
+    @classmethod
+    def upsert(cls,
+               hasAward: 'Award',
+               yearHeld: int,
+               hasEditionNumber: int,
+               yearScreened: int) -> 'AwardCeremony':
+        kwds = {
+            'hasAward': hasAward,
+            'yearHeld': yearHeld,
+            'hasEditionNumber': hasEditionNumber,
+            'yearScreened': yearScreened
+        }
+        return super().upsert(**kwds)
+
+
+def read_wiki_oscar_categories(filename: str) -> typing.List[str]:
+    lst = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = file.read().splitlines()
+    for line in lines:
+        if not line:
+            continue
+        category = line.split(':')[0]
+        lst.append(category)
+    return lst
+
+
+class AwardCategory(models.Model, UpsertMixin):
+    wiki_categories: typing.ClassVar[typing.List[str]] = read_wiki_oscar_categories(
+        ROOT_DIR / 'mapping/oscar-categories.txt')
+
     # object properties
-    forOccupation = models.ForeignKey(Occupation, on_delete=models.CASCADE)
+    forOccupation = models.ForeignKey(Occupation, on_delete=models.CASCADE, null=True)
+
+    # temp
+    label = models.CharField(max_length=255)
+
+    @classmethod
+    def get_instance_from_kaggle_oscar_data(cls, category: str) -> 'AwardCategory':
+        return cls.upsert(label=OSCAR_MAPPING[category])
+
+    @classmethod
+    def get_instance_from_kaggle_bafta_data(cls, category: str) -> 'AwardCategory':
+        matched_str = re.match(r'^Film | (?P<category>.*)in (.*$)', category)
+        return AwardCategory.upsert(label=matched_str['category'])
+        # return cls
 
 
 class Award(models.Model):
     # object properties
-    hasAwardCategory = models.ForeignKey(AwardCategory, on_delete=models.CASCADE)
-    hasPart = models.ForeignKey(AwardCeremony, on_delete=models.CASCADE)
-    hasSetting = models.ForeignKey(NominationSituation, on_delete=models.CASCADE)
-    presentedBy = models.ForeignKey('Organization', on_delete=models.CASCADE)
+    hasAwardCategory = models.ForeignKey(AwardCategory, on_delete=models.CASCADE, null=True)
+    hasPart = models.ForeignKey(AwardCeremony, on_delete=models.CASCADE, null=True)
+    hasSetting = models.ForeignKey(NominationSituation, on_delete=models.CASCADE, null=True)
+    presentedBy = models.ForeignKey('Organization', on_delete=models.CASCADE, null=True)
 
     # data properties
-    hasNickname = models.CharField(max_length=255)
+    hasNickname = models.CharField(max_length=255, null=True)
 
-
-class Audience(models.Model):
     # temp
     label = models.CharField(max_length=255)
 
 
-class Organization(models.Model):
+class Audience(models.Model, UpsertMixin):
+    # temp
+    label = models.CharField(max_length=255)
+
+
+class Organization(Agent):
+    # data properties
+    hasName = models.CharField(max_length=255)
     # temp
     label = models.CharField(max_length=255)
 
 
 class MovieStudio(Organization):
-    locatedIn = models.ForeignKey('Place', on_delete=models.CASCADE)
+    locatedIn = models.ForeignKey('Place', on_delete=models.CASCADE, null=True)
 
 
 class Film(models.Model):
     # object properties
-    hasAudience = models.ForeignKey(Audience, on_delete=models.CASCADE)
-    hasContentRating = models.ForeignKey(ContentRating, on_delete=models.CASCADE)
-    hasCountryOfOrigin = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='origin_country_of_films')
-    hasFilmingLocation = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='filming_location_of_films')  # may not be in use
-    hasGenre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    hasLanguage = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='language_of_films')
-    hasOriginalLanguage = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='original_language_of_films')
+    # hasAudience = models.ForeignKey(Audience, on_delete=models.CASCADE, null=True)
+    # TODO:  fetched from wikidata
+    hasContentRating = models.ManyToManyField(ContentRatingCategory, null=True)
+    # TODO:  fetched from wikidata
+    hasCountryOfOrigin = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='origin_country_of_films',
+                                           null=True)
+    # TODO:  fetched from wikidata
+    hasFilmingLocation = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='filming_location_of_films',
+                                           null=True)  # may not be in use
+    hasGenre = models.ManyToManyField(Genre, null=True)
+    # TODO: wikidata, imdb.TitleBasics
+    hasLanguage = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='language_of_films', null=True)
+
+    hasOriginalLanguage = models.ForeignKey(Language, on_delete=models.CASCADE,
+                                            related_name='original_language_of_films', null=True)
     hasPrequels = models.ForeignKey('Film', on_delete=models.SET_NULL, related_name='hasSequels', null=True)
-    # hasSequels = models.ForeignKey('Film', on_delete=models.SET_NULL)
-    hasSubTitleInLanguage = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='subtitle_of_films')
-    eligibleFor = models.ForeignKey(Award, on_delete=models.CASCADE)
+    # TODO: wikidata
+    # hasSequels = models.ForeignKey('Film', on_delete=models.SET_NULL, null=True)
+    # hasSubTitleInLanguage = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='subtitle_of_films', null=True)
+    # eligibleFor = models.ForeignKey(Award, on_delete=models.CASCADE, null=True)
 
     # data properties
-    dateReleased = models.CharField(max_length=255)  # models.DateField()
-    hasFeatureLengthInMinutes = models.IntegerField()
-    hasInitialReleaseYear = models.IntegerField()
-    hasTitle = models.CharField(max_length=255)
-    hasWikipediaLink = models.CharField(max_length=255)  # models.URLField()
-    isBritishFilm = models.BooleanField()
-    isAdult = models.BooleanField()
+    dateReleased = models.CharField(max_length=255, null=True)  # models.DateField(, null=True)
+    hasFeatureLengthInMinutes = models.IntegerField(null=True)
+    hasInitialReleaseYear = models.IntegerField(null=True)
+    hasTitle = models.CharField(max_length=255, null=True)
+    hasWikipediaLink = models.CharField(max_length=255, null=True)  # models.URLField(, null=True)
+    # isBritishFilm = models.BooleanField(null=True)
+    isAdult = models.BooleanField(null=True)
+
+    # wikidata
+    hasWikidataId = models.CharField(null=True, max_length=13)
 
     # temp
-    avg_rating = models.CharField(max_length=255)
+    avg_rating = models.CharField(max_length=255, null=True)
+    t_const = models.CharField(max_length=255, unique=True)
+
+    def retrieve_content_rating_from_wikidata(self):
+        for cr in get_content_ratings_for_film(self.hasWikidataId):
+            cr_model = ContentRating.upsert(appliesInCountry=Country.upsert(name=cr.appliesInCountry))
+            category = ContentRatingCategory.upsert(hasValue=cr.value, isPartOf=cr_model)
+            self.hasContentRating.add(category)
+
+    def retrieve_country_of_origin_from_wikidata(self):
+        f = get_single_valued_prop(self.hasWikidataId)
+        self.dateReleased = f.hasPublicationDate
+        self.hasCountryOfOrigin = f.hasCountryOfOrigin
+        self.hasOriginalLanguage = f.hasOriginalLanguage
 
 
 class Character(models.Model):
     # object properties
-    actedBy = models.ForeignKey(Person, on_delete=models.CASCADE)
-    hasGender = models.CharField(max_length=255, choices=Gender.choices)
-    hasImportance = models.CharField(max_length=255, choices=CharacterImportance.choices)
+    actedBy = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
+    hasGender = models.CharField(max_length=255, choices=Gender.choices, null=True)
+    hasImportance = models.CharField(max_length=255, choices=CharacterImportance.choices, null=True)
 
     # data properties
-    hasCharacterTitle = models.CharField(max_length=255)
-    hasName = models.CharField(max_length=255)
+    # hasCharacterTitle = models.CharField(max_length=255, null=True)
+    hasName = models.CharField(max_length=255, null=True)
