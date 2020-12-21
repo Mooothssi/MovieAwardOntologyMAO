2403d8d65e3ef22a6fde61a3143c760b9e68284c
T. Bunratta <tharathorn.bu@ku.th>
2020-12-21 12:42:07 +0700

    Update populate_onto.py

--- populate_onto.py
+++ populate_onto.py
@@ -4,29 +4,35 @@ from pprint import pprint
 
 from django.db.models import Field, AutoField, ManyToOneRel
 
+from app.models import Genre
 from ontogen.converter import OntogenConverter
+from ontogen.mixins.base import DjModelOntogenMixin
 from ontogen.primitives.properties import OwlProperty, OwlObjectProperty
 from ontogen.utils.basics import absolutize_entity_name
-from start_dj import start_django_lite
+import start_dj
 from dirs import ROOT_DIR
 from django.db import models
 
-from ontogen import OwlClass, OwlIndividual, Ontology
+from ontogen import Ontology
 
-start_django_lite()
-
-from app.models import Film, Country
-
-from ontogen.mixins import model_to_entity
+from ontogen.wrapper import all_subclasses
 
 
 def add_individuals(converter: OntogenConverter) -> None:
-    for f in Country.objects.all():
-        indiv = model_to_entity(converter.ontology, f)
-        converter.ontology.add_entity(indiv)
-    for f in Film.objects.all():
-        indiv = model_to_entity(converter.ontology, f)
-        converter.ontology.add_entity(indiv)
+    for cls in all_subclasses(DjModelOntogenMixin):
+        if cls == DjModelOntogenMixin:
+            continue
+        if cls == Genre:
+            print()
+        for inst in cls.objects.all():
+            indiv = cls.model_to_entity(converter.ontology, inst)
+            converter.ontology.add_entity(indiv)
+    # for f in Country.objects.all():
+    #     indiv = model_to_entity(converter.ontology, f)
+    #     converter.ontology.add_entity(indiv)
+    # for f in Film.objects.all():
+    #     indiv = model_to_entity(converter.ontology, f)
+    #     converter.ontology.add_entity(indiv)
 
 
 if __name__ == '__main__':
