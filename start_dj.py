ec57f62ac7c86ef3284f44b9db71bcb917286261
T. Bunratta <tharathorn.bu@ku.th>
2020-12-21 12:42:19 +0700

    Update start_dj.py

--- start_dj.py
+++ start_dj.py
@@ -4,11 +4,6 @@ from django.apps import apps
 from django.conf import settings
 
 
-def start_django_lite():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mao_dj.mysite.settings")
-    if not apps.loading:
-        apps.populate(settings.INSTALLED_APPS)
-
-
-if __name__ == '__main__':
-    start_django_lite()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mao_dj.mysite.settings")
+if not apps.loading:
+    apps.populate(settings.INSTALLED_APPS)
