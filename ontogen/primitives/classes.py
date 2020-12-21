f0223e568c3bf1a624f6a87630fdda42e8e5ad0f
T. Bunratta <tharathorn.bu@ku.th>
2020-12-21 10:45:50 +0700

    Fix `str` check in primitives/classes

--- ontogen/primitives/classes.py
+++ ontogen/primitives/classes.py
@@ -81,7 +81,7 @@ class OwlIndividual(OwlEntity, OwlAssertable):
                     return self._prepare_assertion_value(prop_name, value[0])
             return [self._prepare_assertion_value(prop_name, v) for v in value]
         else:
-            if isinstance(val, str) and prop_name in self.all_defined_properties:
+            if prop_name in self.all_defined_properties:
                 prop = self.all_defined_properties[prop_name]
                 if not prop:
                     return value
